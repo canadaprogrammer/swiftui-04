f9f2a5ee2ea681be5bf8cca91ef01d6c9a9fc468
Jin Park <jinpark1504@gmail.com>
2024-04-15 11:16:43 +0900

    불투명반환타입(Opaque Return Type)

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -17,3 +17,24 @@ class MyClass: MessageBuilder {
         return "Hello " + name
     }
 }
+
+// some : 안에 있는 반환타입이 무엇인지를 안 알려주기 위해서 사용. 접근도 막아주는 표과
+// Equatable : 비교연산자를 사용할 수 있는 protocol
+
+func doubleFunc1 (value: Int) -> some Equatable {
+    return value * 2
+}
+
+func doubleFunc2 (value: String) -> some Equatable {
+    value  + value
+}
+
+let intOne = doubleFunc1(value: 10)
+print(intOne)
+let stringOne = doubleFunc2(value: "Hello")
+print(stringOne)
+
+// 동작하지 않음
+//if (intOne == stringOne)  {
+//    
+//}
