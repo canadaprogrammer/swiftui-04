7aeb3e2ad543f280638de9a0c89c56d52149392f
Jin Park <jinpark1504@gmail.com>
2024-04-16 10:49:38 +0900

    property wrapper with generic

--- MyPlayground13.playground/Contents.swift
+++ MyPlayground13.playground/Contents.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 //struct Address {
 //    private var cityname: String = ""
@@ -37,3 +38,72 @@ var contact = Contact(name: "John Smith", city: "London", country: "United Kingd
 print("\(contact.name) \(contact.city) \(contact.country)")
 
 // 프로퍼티 랩퍼를 선언하여 반복적으로 사용
+//
+//@propertyWrapper
+//struct MinMaxVal {
+//    var value: Int
+//    var min: Int
+//    var max: Int
+//    
+//    init(wrappedValue: Int, min: Int, max: Int) {
+//        value = wrappedValue
+//        self.min = min
+//        self.max = max
+//    }
+//    
+//    var wrappedValue: Int {
+//        get { return value }
+//        set {
+//            if newValue > max {
+//                value = max
+//            } else if newValue < min {
+//                value = min
+//            } else {
+//                value = newValue
+//            }
+//        }
+//    }
+//}
+
+// generic
+@propertyWrapper
+struct MinMaxVal<V: Comparable> {
+    var value: V
+    var min: V
+    var max: V
+    
+    init(wrappedValue: V, min: V, max: V) {
+        value = wrappedValue
+        self.min = min
+        self.max = max
+    }
+    
+    var wrappedValue: V {
+        get { return value }
+        set {
+            if newValue > max {
+                value = max
+            } else if newValue < min {
+                value = min
+            } else {
+                value = newValue
+            }
+        }
+    }
+}
+struct Demo {
+    @MinMaxVal(min: 10, max: 150) var value: Int = 100
+    @MinMaxVal(min: "Apple", max: "Orange") var value2: String = "Banana"
+    @MinMaxVal(min: Date(), max: Calendar.current.date(byAdding: .month, value: 1, to: Date())! ) var value3: Date = Date()
+}
+
+var demo = Demo()
+print(demo.value2)
+demo.value2 = "Pear"
+print(demo.value2)
+
+print(demo.value3)
+demo.value3 = Calendar.current.date(byAdding: .day, value: 3, to: Date())!
+print(demo.value3)
+demo.value3 = Calendar.current.date(byAdding: .month, value: 2, to: Date())!
+print(demo.value3)
