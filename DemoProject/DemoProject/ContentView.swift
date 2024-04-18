7a359c5747f96a02a636aa034e65d00ebc9d88f0
Jin Park <jinpark1504@gmail.com>
2024-04-18 09:38:46 +0900

    후행 클로저

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -7,25 +7,17 @@
 
 import SwiftUI
 
-struct StandardTitle: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .background(Color.white)
-            .border(Color.gray, width: 0.2)
-            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-    }
-}
 struct ContentView: View {
     var body: some View {
         VStack {
-            Spacer()
-            Text("Text 1")
-                .modifier(StandardTitle())
-            Spacer()
-            Text("Text 1")
-                .modifier(StandardTitle())
-            Spacer()
+//            Button {
+//                print("Clicked")
+//            } label: {
+//                Text("Button")
+//            }
+            Button("Button", systemImage: "tortoise") {
+                print("Clicked")
+            }
         }
     }
 }
