f47ac04e238733bccf46c9cb60d7c1ab80194d12
Jin Park <jinpark1504@gmail.com>
2024-04-18 09:50:00 +0900

    Custom Container View

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -7,16 +7,31 @@
 
 import SwiftUI
 
+struct MyVStack<Content: View>: View {
+    let content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            content()
+        }
+        .font(.largeTitle)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-//            Button {
-//                print("Clicked")
-//            } label: {
-//                Text("Button")
-//            }
-            Button("Button", systemImage: "tortoise") {
-                print("Clicked")
+        MyVStack {
+            Text("Text 1")
+            Text("Text 2")
+            Text("Text 3")
+            HStack {
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
             }
         }
     }
