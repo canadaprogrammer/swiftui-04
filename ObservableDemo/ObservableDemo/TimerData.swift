5d8a8af909b01876de951a50a19c1a0f26a132b5
Jin Park <jinpark1504@gmail.com>
2024-04-19 17:17:04 +0900

    TimerData

--- ObservableDemo/ObservableDemo/TimerData.swift
+++ ObservableDemo/ObservableDemo/TimerData.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Combine
+
+class TimerData: ObservableObject {
+    @Published var timeCount = 0
+    var timer: Timer?
+    
+    init() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerDidFire), userInfo: nil, repeats: true)
+    }
+    
+    // selector 에 함수를 보내야 해서 메크로 지정을 해줌
+    @objc func timerDidFire() {
+        timeCount += 1
+    }
+    
+    func resetCount() {
+        timeCount = 0
+    }
+}
