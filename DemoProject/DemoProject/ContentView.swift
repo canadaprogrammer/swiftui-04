684b7bc59a01fe5bdb2fe7bda8df10966fe37a01
Jin Park <jinpark1504@gmail.com>
2024-04-19 14:24:35 +0900

    async / await - another example

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -7,33 +7,45 @@
 
 import SwiftUI
 
+struct Repository: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String?
+}
+
 struct ContentView: View {
+    @State private var username = "jmbae"
+    @State private var repositories: [Repository] = []
     var body: some View {
         VStack {
-            Button(action: {
-               doSomething()
-            }) {
-                Text("Do Something")
+            TextField("Github username:", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button("Fetch Data") {
+                Task {
+                    do {
+                        async let fetchRepositoris = fetchRepositories(username: username)
+                        repositories = try await fetchRepositoris
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                }
+            }
+            List(repositories) { repo in
+                VStack(alignment: .leading) {
+                    Text(repo.name)
+                        .font(.headline)
+                    Text(repo.description ?? "No description")
+                        .font(.subheadline)
+                }
             }
         }
     }
     
-    func doSomething() {
-        print("Start \(Date())")
-        let detachedTask = Task.detached {
-//            let result = await takesTooLong()
-//            print("Date result: \(result)")
-            async let result = takesTooLong()
-            print("Date result: \(await result)")
-        }
-        print(detachedTask.isCancelled)
-        print("End \(Date())")
-    }
-    
-    func takesTooLong() async -> Date {
-        sleep(5)
-        print("Async task completed at \(Date())")
-        return Date()
+    func fetchRepositories(username: String) async throws -> [Repository] {
+        let url = URL(string: "https://api.github.com/users/\(username)/repos")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([Repository].self, from: data)
     }
 }
 #Preview {
