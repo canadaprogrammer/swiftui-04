56daf02b5e8a00f62f1ec836ce25488b50f60fac
Jin Park <jinpark1504@gmail.com>
2024-04-19 17:12:25 +0900

    Initialize Project ObservableDemo

---
+++ ObservableDemo/ObservableDemo/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  ObservableDemo
+//
+//  Created by Jungjin Park on 2024-04-19.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObservableDemo/ObservableDemo/TimerData.swift
@@ -0,0 +1,8 @@
+//
+//  TimerData.swift
+//  ObservableDemo
+//
+//  Created by Jungjin Park on 2024-04-19.
+//
+
+import Foundation
