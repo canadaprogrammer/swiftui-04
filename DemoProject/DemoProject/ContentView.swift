0fb9f1a0a611a41a474475df63fed242be33e793
Jin Park <jinpark1504@gmail.com>
2024-04-19 09:27:46 +0900

    clear to start ObservableObject

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -8,21 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wifiEnabled = true
-    @State private var userName = ""
-    let textLimit = 5
     var body: some View {
         VStack {
-            Toggle(isOn: $wifiEnabled) {
-                Text("Enable Wi-Fi")
-            }
-            TextField("Enter user name", text: $userName)
-            Text(userName
-                .prefix(textLimit))
-            Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
-                .font(.largeTitle)
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 10)
+            
         }
     }
 }
