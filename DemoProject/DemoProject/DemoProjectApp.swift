85cf54863a9688e66b39d19df34e32b3b451787e
Jin Park <jinpark1504@gmail.com>
2024-04-19 17:02:01 +0900

    scenePhase onChange

--- DemoProject/DemoProject/DemoProjectApp.swift
+++ DemoProject/DemoProject/DemoProjectApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct DemoProjectApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { (oldScenePhase, newScenePhase) in
+            switch newScenePhase {
+            case .active:
+                print("Active \(oldScenePhase)")
+            case .inactive:
+                print("Inctive \(oldScenePhase)")
+            case .background:
+                print("Background")
+            default:
+                print("Unknown scenephase")
+            }
         }
     }
 }
