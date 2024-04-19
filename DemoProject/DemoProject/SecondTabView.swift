066712ab38d8983ebc930aa08188b4457b710c90
Jin Park <jinpark1504@gmail.com>
2024-04-19 16:31:23 +0900

    onChange

--- DemoProject/DemoProject/SecondTabView.swift
+++ DemoProject/DemoProject/SecondTabView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct SecondTabView: View {
+    @State private var text: String = ""
+    
     var body: some View {
-        Text("View Two")
+        TextEditor(text: $text)
+            .padding()
+            .onChange(of: text) {
+                print("onChange triggered \($0) \($1)")
+            }
     }
 }
 
