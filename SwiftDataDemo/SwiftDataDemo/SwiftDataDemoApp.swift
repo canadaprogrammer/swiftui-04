3ee31f9ef5e0c825ce4a79a4f4c7cb7fddec98f0
Jin Park <jinpark1504@gmail.com>
2024-04-22 13:29:19 +0900

    use dummy data in code

---
+++ SwiftDataDemo/SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Jungjin Park on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
